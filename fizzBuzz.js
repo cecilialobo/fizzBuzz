75d0e2b8d2af1dadf38cad3270ebf4919f5238eb
Cecilia Lobo <cecilialobo92@gmail.com>
2021-03-01 18:48:43 +0100

    Remove global variable and correct toString implementation

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,22 +1,24 @@
 const fizzBuzz = (n) => {
-    listOfIntegers = [];
+    const listOfIntegers = [];
+    
     for (let i = 1; i <= n; i++) {
-        let value = toString(i);
-
+        let value = i.toString();
+        
         if (i % 3 === 0) {
             value = 'Fizz';
-        }
-
+        } 
+        
         if (i % 5 === 0) {
             value = 'Buzz';
         }
-        
+
         if (i % 3 === 0 && i % 5 === 0) {
             value = 'FizzBuzz';
-        }
-
+        } 
+        
         listOfIntegers.push(value);
     }
+
     return listOfIntegers;
 };
 
--- fizzBuzz.spec.js
+++ fizzBuzz.spec.js
@@ -1,6 +1,11 @@
 const fizzBuzz = require('./fizzBuzz.js');
 
 test('if n = 15 expects listOfIntegers to be equal to ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]', () => {
-    listOfIntegers = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"];
-    expect(fizzBuzz(15)).toEqual(listOfIntegers);
+    const result = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"];
+    expect(fizzBuzz(15)).toEqual(result);
+});
+
+test('if n = 15 expects listOfIntegers to be equal to ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]', () => {
+    const result = ["1", "2", "Fizz"];
+    expect(fizzBuzz(3)).toEqual(result);
 });
